29169efe43d75aab5297736247ac545e4b628c17
Simone Sestito <simone@simonesestito.com>
2025-04-14 16:03:19 +0200

    Add script to run on DI cluster

---
+++ run_job_on_di_cluster.sh
@@ -0,0 +1,54 @@
+#!/bin/bash --login
+set -eo pipefail
+### NOTE
+### This script is meant to be run on the DI cluster.
+###
+### Change the job name and partition
+
+# Job name:
+#SBATCH --job-name=pag-llm
+
+# Define the partition on which the job shall run
+#SBATCH --partition department_only
+
+# Number of processes.
+# Unless programmed using MPI,
+# most programs using GPU-offloading only need
+# a single CPU-based process to manage the device(s)
+#SBATCH --ntasks=1
+
+# Type and number of GPUs
+# The type is optional.
+#SBATCH --gpus=1
+
+# Total CPU memory
+# All available memory per GPU is allocated by default.
+# Specify "M" or "G" for MB and GB respectively
+#SBATCH --mem=8G
+
+# Wall time
+# Format: "minutes", "hours:minutes:seconds",
+# "days-hours", or "days-hours:minutes"
+#SBATCH --time=00:02:00
+
+# Standard output and error to file
+# %x: job name, %j: job ID
+#SBATCH --output=%x-%j.SLURMout
+
+echo "Running with the following arguments:"
+echo "Job name: $SLURM_JOB_NAME"
+echo "Job ID: $SLURM_JOB_ID"
+echo "Git commit: $(git rev-parse HEAD)"
+echo
+echo "User-specified arguments:"
+printf '%s\n' "$@"
+echo
+echo "========================="
+echo
+echo
+
+cd "/home/$USER/pag-llm"
+source .venv/bin/activate
+
+srun python "$@"
+
