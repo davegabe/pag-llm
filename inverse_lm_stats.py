f49845310c995dfa0b800d61f8deb9e5f280df70
Simone Sestito <simone@simonesestito.com>
2025-04-24 11:26:05 +0200

    Set float32 matmul precision

--- inverse_lm_stats.py
+++ inverse_lm_stats.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import torch
 from lightning import Trainer
 
 from config import CustomLLMPagConfig, apply_config
@@ -8,14 +9,15 @@ from instantiate import load_model_from_checkpoint
 
 @apply_config('inv-first-tiny-train')
 def main(cfg: CustomLLMPagConfig):
+    torch.set_float32_matmul_precision('medium')
+
     lightning_module, data_module, module_name, cfg = load_model_from_checkpoint(
         pathlib.Path('./checkpoints/tinystories/tinystories_identity_grad_norm__qp6q1mop.ckpt'),
         cfg,
     )
-    print(lightning_module)
 
     trainer = Trainer(devices=cfg.training.device if cfg.training.device else "auto")
-    trainer.test(lightning_module, data_module)
+    trainer.validate(lightning_module, data_module)
 
 
 if __name__ == '__main__':
