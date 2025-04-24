8c14a83ecff6b468c49d46dd6386cd66d5aeaadc
davegabe <me@davegabe.it>
2025-04-24 21:23:19 +0200

    Fixed inverse_lm_stats to proper feed the model the sequence with masked token

--- inverse_lm_stats.py
+++ inverse_lm_stats.py
@@ -2,14 +2,13 @@ import pathlib
 from collections import defaultdict
 
 import torch
-import torch.nn.functional as F
 from lightning import Trainer
 from tqdm import tqdm
 
 from config import CustomLLMPagConfig, apply_config
 from data.data_processor import BatchType
 from instantiate import load_model_from_checkpoint
-from models.common import compute_top_k_accuracies
+from models.common import compute_top_k_accuracies, forward_grad_embeddings
 
 
 @apply_config('inv-first-tiny-train')
@@ -27,27 +26,33 @@ def main(cfg: CustomLLMPagConfig):
 
     # Do a forward testing
     trainer = Trainer(devices='0,')
-    trainer.validate(lightning_module, data_module.test_dataloader())
+    # trainer.validate(lightning_module, data_module.test_dataloader())
 
     overall_accuracy = defaultdict(int)
+    lightning_module.eval()
 
-    for batch in tqdm(data_module.train_dataloader()):
+    for batch in tqdm(data_module.val_dataloader()):
         batch: BatchType = batch.to(torch.device(device))
         input_ids, attention_mask, shift_labels = batch.input_ids, batch.attention_mask, batch.shift_labels
 
-        # Get the embeddings of X
-        x_embed = lightning_module.model.get_input_embeddings()(input_ids).detach()
-        x_embed.requires_grad_(True)
+        # Send to device
+        input_ids = input_ids.to(device)
+        attention_mask = attention_mask.to(device)
+        shift_labels = shift_labels.to(device)
 
         for k in range(prefix_len - 1, -1, -1):
             # Do a forward pass,
             # letting the model see only the tokens after k-th.
             # The model must predict token k-th
             # To do that, we need to mask the k-th token with [PAD]
-            original_k_token = input_ids[:, k]
+            original_k_token = input_ids[:, k].clone()
 
             # Replace the k-th token with [PAD]
             input_ids[:, k] = lightning_module.tokenizer.pad_token_id
+
+            # Get the embeddings of X (with the k-th token replaced with [PAD])
+            x_embed = lightning_module.model.get_input_embeddings()(input_ids).detach()
+            x_embed.requires_grad_(True)
 
             outputs = lightning_module.model(
                 inputs_embeds=x_embed[:, k:],
@@ -61,16 +66,12 @@ def main(cfg: CustomLLMPagConfig):
             input_ids[:, k] = original_k_token
 
             # Predict the k-th token, based on the gradients of the first token embeddings
-            grad_x_embed = lightning_module.model.model.norm(grad_x_embed)
-            input_ids_predicted = F.linear(
-                input=grad_x_embed,
-                weight=lightning_module.model.lm_head.weight,
-            )
+            logits = forward_grad_embeddings(lightning_module.model, grad_x_embed)
 
             # Compute the top-k accuracies
             top_k_accuracies = compute_top_k_accuracies(
                 inv_first_label=original_k_token,
-                logits=input_ids_predicted,
+                logits=logits,
                 k_samples=10,
                 tag='test',
             )
