986d668e1e25c038555d2daff72cc5dc835f6c0a
Simone Sestito <simone@simonesestito.com>
2025-06-18 08:49:26 +0200

    GCG: attack the new identity_grad with sum (X + grad_X)

--- run_gcg.py
+++ run_gcg.py
@@ -175,7 +175,7 @@ def main(cfg: CustomLLMPagConfig):
         'base': 'tinystories_base__cs1bklll.ckpt',
         'bert-like': 'tinystories_bertlike_embeddings_grad_norm__sqipem6p.ckpt',
         'inv-first': 'tinystories_inv_first_norm__9ecoqzxt.ckpt',
-        'identity-grad': 'tinystories_identity_grad_norm__qp6q1mop.ckpt',
+        'identity-grad': 'tinystories_identity_grad_sum__1irckidh.ckpt',
     }[cfg.training.method]
     lightning_model, data_module, model_name, cfg = load_model_from_checkpoint(
         cfg.model.output_dir / ckpt_file,
