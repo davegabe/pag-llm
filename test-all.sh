bb7880bd543fe4476bfc28dc1d33e819989209b1
davegabe <me@davegabe.it>
2025-09-16 02:13:56 +0200

    Update .gitignore and refactor test-all.sh to use PYTHON_SCRIPT variable

--- test-all.sh
+++ test-all.sh
@@ -1,14 +1,17 @@
+# PYTHON_SCRIPT=test.py
+PYTHON_SCRIPT=eval_gcg.py
+
 # Base
-python test.py --config-name base-small-offline +model.checkpoint_path=best-checkpoints/best-base.ckpt
+python $PYTHON_SCRIPT --config-name base-small-offline +model.checkpoint_path=best-checkpoints/best-base.ckpt
 
 # Bert like
-python test.py --config-name pag-identity-small-offline +model.checkpoint_path=best-checkpoints/best-bert-like.ckpt training.method=bert-like
-python test.py --config-name pag-identity-small-offline +model.checkpoint_path=best-checkpoints/best-grad-bert-like.ckpt training.method=val-bert-like
+python $PYTHON_SCRIPT --config-name pag-identity-small-offline +model.checkpoint_path=best-checkpoints/best-bert-like.ckpt training.method=bert-like
+python $PYTHON_SCRIPT --config-name pag-identity-small-offline +model.checkpoint_path=best-checkpoints/best-grad-bert-like.ckpt training.method=val-bert-like
 
 # Inv first
-python test.py --config-name inv-first-small-offline +model.checkpoint_path=best-checkpoints/best-inv-first.ckpt training.method=inv-first
-python test.py --config-name inv-first-small-offline +model.checkpoint_path=best-checkpoints/best-grad-inv-first.ckpt training.method=val-inv-first
+python $PYTHON_SCRIPT --config-name inv-first-small-offline +model.checkpoint_path=best-checkpoints/best-inv-first.ckpt training.method=inv-first
+python $PYTHON_SCRIPT --config-name inv-first-small-offline +model.checkpoint_path=best-checkpoints/best-grad-inv-first.ckpt training.method=val-inv-first
 
 # Pag identity
-python test.py --config-name pag-identity-small-offline +model.checkpoint_path=best-checkpoints/best-pag-identity.ckpt training.method=identity-grad
-python test.py --config-name pag-identity-small-offline +model.checkpoint_path=best-checkpoints/best-grad-identity.ckpt training.method=val-identity-grad
+python $PYTHON_SCRIPT --config-name pag-identity-small-offline +model.checkpoint_path=best-checkpoints/best-pag-identity.ckpt training.method=identity-grad
+python $PYTHON_SCRIPT --config-name pag-identity-small-offline +model.checkpoint_path=best-checkpoints/best-grad-identity.ckpt training.method=val-identity-grad
