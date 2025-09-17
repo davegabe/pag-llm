293622ae88fd36a714dee1d7cf25f5909418de51
davegabe <me@davegabe.it>
2025-09-17 16:17:23 +0200

    Fix typo in gcg-eval-all.sh for val-identity-grad method checkpoint path

--- run-all/gcg-eval-all.sh
+++ run-all/gcg-eval-all.sh
@@ -3,7 +3,7 @@ python eval_gcg.py --config-name base-small-offline training.method=base +model.
 
 # Identity
 python eval_gcg.py --config-name pag-identity-small-offline training.method=identity-grad +model.checkpoint_path=best-checkpoints/best-identity-grad.ckpt
-python eval_gcg.py --config-name pag-identity-small-offline training.method=val-identity-grad +model.checkpoint_path=best-checkpoints/best-val-identity.ckpt
+python eval_gcg.py --config-name pag-identity-small-offline training.method=val-identity-grad +model.checkpoint_path=best-checkpoints/best-val-identity-grad.ckpt
 
 # Bert like
 python eval_gcg.py --config-name pag-identity-small-offline training.method=bert-like +model.checkpoint_path=best-checkpoints/best-bert-like.ckpt
