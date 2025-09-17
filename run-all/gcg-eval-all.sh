904af8e29d631863fbe0ae843aa4f7bd75cfc3c4
davegabe <me@davegabe.it>
2025-09-17 16:16:10 +0200

    Add gcg-eval-all.sh script for evaluating various methods

---
+++ run-all/gcg-eval-all.sh
@@ -0,0 +1,14 @@
+# Base
+python eval_gcg.py --config-name base-small-offline training.method=base +model.checkpoint_path=best-checkpoints/best-base.ckpt
+
+# Identity
+python eval_gcg.py --config-name pag-identity-small-offline training.method=identity-grad +model.checkpoint_path=best-checkpoints/best-identity-grad.ckpt
+python eval_gcg.py --config-name pag-identity-small-offline training.method=val-identity-grad +model.checkpoint_path=best-checkpoints/best-val-identity.ckpt
+
+# Bert like
+python eval_gcg.py --config-name pag-identity-small-offline training.method=bert-like +model.checkpoint_path=best-checkpoints/best-bert-like.ckpt
+python eval_gcg.py --config-name pag-identity-small-offline training.method=val-bert-like +model.checkpoint_path=best-checkpoints/best-val-bert-like.ckpt
+
+# Inv first
+python eval_gcg.py --config-name inv-first-small-offline training.method=inv-first +model.checkpoint_path=best-checkpoints/best-inv-first.ckpt
+python eval_gcg.py --config-name inv-first-small-offline training.method=val-inv-first +model.checkpoint_path=best-checkpoints/best-val-inv-first.ckpt
